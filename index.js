d5975296858d407f9ad9ac67f7f2b1a8961fe0c2
Labubu007 <xz5414@nyu.edu>
2025-10-31 00:25:12 -0400

    10/31 update

---
+++ index.js
@@ -0,0 +1,8 @@
+//document.getElementById("index.html").innerText= "Welcome to CMPND Study Room";
+
+//let message = "Welcome to CMPND Study Room";
+//console.log("Welcome to CMPND Study Room");
+
+function incrementCounter() {
+    console.log("Increment button clicked");
+}
